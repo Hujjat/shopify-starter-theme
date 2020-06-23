54eedb3c136c89d599d9a95b113cfedcf620369f
pat-white-code <pat.k.white@gmail.com>
2020-06-23 09:46:19 -0500

    makes responsive logo

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,6 @@
 
 import Vue from "vue";
 
-
-var header = new Vue({
-    el: "#header"
-})
-
-
-var minicart = new Vue({
-    el: "#minicart"
-})
-
-
-var login = new Vue({
-    el: "#login"
-})
-
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
